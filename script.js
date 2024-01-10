3af2c82dc46bba7115415ea11d6d8748de9e697c
Jeevana <109099313+Jeevana-24@users.noreply.github.com>
2024-01-09 18:33:50 -0800

    dom manupulation

--- script.js
+++ script.js
@@ -2,44 +2,102 @@ function getComputerChoice(){
     choices=["rock","paper","scissors"]
     return choices[Math.floor((Math.random()*3))];
 }
-function getUserChoice(){
-    return window.prompt('rock,paper or scissors?')
-}
-
+const bodie=document.querySelector('body');
+const para = document.createElement("p");
+bodie.appendChild(para);
+const l=[]
 function plays(playerSelection,computerSelection){
-    if (playerSelection==="rock" && computerSelection==="scissors"){
-        console.log("you win!")
+    if (playerSelection ==="rock" && computerSelection === "scissors"){
+        x="you win!";
     }
-    else if(playerSelection==="scissors" && computerSelection==="paper"){
-        console.log("you win!")
+    else if(playerSelection ==="scissors" && computerSelection === "paper"){
+        x="you win!";
     }
     else if(playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log('You win!')
+        x="you win!";
     }
     
     else if(playerSelection === 'scissors' && computerSelection === 'rock') {
-        console.log('You lose!')
+        x='you lose!';
     }
     
     else if(playerSelection === 'paper' && computerSelection === 'scissors') {
-        console.log('You lose!')
+        x='you lose!';
     }
     
     else if (playerSelection === 'rock' && computerSelection === 'paper') {
-        console.log('You lose!')
+        x='you lose!';
     }
     else{
-        console.log("tie")
+        x="tie"
     }
-}
-const l=[];
-function Game(){
-    for(let i=0;i<5;i++){
-        x=plays(getUserChoice(),getComputerChoice());
-        l.push(x)
+    
+    l.push(x)
+    console.log(l)
+    if (l.length==5){
+        let win=0
+        let lose=0
+        for(let i=0;i<l.length;i++){
+            if (l[i]=="you win!"){
+                win=win+1
+            }
+            else{
+                lose=lose+1
+            }
+        }
+        if (win>lose){
+            
+            alert("You win the game");
+        }
+        else{
+            alert("Computer wins the game");
+        }
+    
     }
+    return x;
 }
-let win=0
+let playerSelection;
+const body=document.querySelector('body');
+const btn1 = document.createElement("button");
+btn1.textContent = "rock";
+body.appendChild(btn1);
+
+
+const btn2 = document.createElement("button");
+btn2.textContent = "paper";
+body.appendChild(btn2);
+
+
+const btn3 = document.createElement("button");
+btn3.textContent = "scissors";
+body.appendChild(btn3);
+
+btn1.onclick=()=>{
+    playerSelection="rock"
+    computerSelection=getComputerChoice()
+x=plays(playerSelection,computerSelection)
+para.textContent =x;
+};
+
+btn2.onclick=()=>{playerSelection="paper"
+computerSelection=getComputerChoice()
+x=plays(playerSelection,computerSelection)
+para.textContent =x;
+};
+
+btn3.onclick=()=>{playerSelection="scissors"
+computerSelection=getComputerChoice()
+x=plays(playerSelection,computerSelection)
+para.textContent =x;
+};
+
+
+
+
+    
+
+
+/*let win=0
 let lose=0
 for(let i=0;i<l.length;i++){
     if (l[i]=="You win!"){
@@ -56,4 +114,4 @@ else{
     console.log("Computer wins the game")
 }
 
-Game();
+Game();*/
